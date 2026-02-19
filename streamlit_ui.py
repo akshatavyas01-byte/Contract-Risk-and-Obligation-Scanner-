9369e5e5b7b16da6a538d4ae161d1e32b72b3a6b
Akshata Vyas <akshatavyas01@gmail.com>
2026-02-19 08:45:15 +0530

    Using Containers in streamlit_ui.py

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -2,11 +2,14 @@ import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 import httpx, json, time
 
-placeholder=st.empty()
-
 if "submitted" not in st.session_state:
     st.session_state.submitted=False
 
+form=st.container()
+stream=st.container()
+
+with stream:
+    output_placeholder = st.empty() 
  
 def Request(file:UploadedFile, filetype:str):
     st.session_state.submitted=True
@@ -21,26 +24,27 @@ def Request(file:UploadedFile, filetype:str):
                     params={"filetype":filetype},
                     files=files,
                     timeout=None) as response:
-     streamed_text=" "  
+     stream_text=""
      for chunks in response.iter_text():
-         streamed_text+=chunks
-         placeholder.markdown(f"`{streamed_text}`")
+         stream_text+=chunks
+         output_placeholder.write(f"{stream_text}")
         
         
+with form:       
+    if not st.session_state.submitted:
+        st.title("Live Contract Risk & Obligation Scanner")
+        st.subheader("Upload File for Scanning")
+        uploaded=st.file_uploader(
+            "Upload File",
+            type=["pdf","txt","docx"],
+            )
+        filetype=st.radio("FILE TYPE",options=["text-pdf","image-pdf","text-file"],index=None)
+        if filetype and uploaded:
+            st.write(f" Done type:{filetype}")
         
-if not st.session_state.submitted:
-    st.title("Live Contract Risk & Obligation Scanner")
-    st.subheader("Upload File for Scanning")
-    uploaded=st.file_uploader(
-        "Upload File",
-        type=["pdf","txt","docx"],
-        )
-    filetype=st.radio("FILE TYPE",options=["text-pdf","image-pdf","text-file"],index=None)
-    if filetype and uploaded:
-        st.write(f" Done type:{filetype}")
+        st.button("CHECK",on_click=Request, args=(uploaded,filetype))
+ 
     
-    st.button("CHECK",on_click=Request, args=(uploaded,filetype))
-       
 
 
     
