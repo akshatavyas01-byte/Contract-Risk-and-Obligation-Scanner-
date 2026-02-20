9e6437db4624a599e992e26213f5cbba00c06716
Akshata Vyas <akshatavyas01@gmail.com>
2026-02-20 10:43:31 +0530

    Redesigned the UI removed containers for better efficiency .

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -1,18 +1,20 @@
 import streamlit as st
-from streamlit.runtime.uploaded_file_manager import UploadedFile
-import httpx, json, time
+import httpx
 
 if "submitted" not in st.session_state:
     st.session_state.submitted=False
 
-form=st.container()
-stream=st.container()
+if "processing" not in st.session_state:
+    st.session_state.processing=False
 
-with stream:
-    output_placeholder = st.empty() 
- 
-def Request(file:UploadedFile, filetype:str):
-    st.session_state.submitted=True
+if "output" not in st.session_state:
+    st.session_state.output=""
+
+if st.session_state.processing:
+    stream_text=""
+    output_placeholder=st.empty()
+    file=st.session_state.uploaded
+    filetype=st.session_state.file_type
     files={
         "file":(
             file.name,
@@ -24,25 +26,46 @@ def Request(file:UploadedFile, filetype:str):
                     params={"filetype":filetype},
                     files=files,
                     timeout=None) as response:
-     stream_text=""
+   
      for chunks in response.iter_text():
-         stream_text+=chunks
+         stream_text+=chunks 
          output_placeholder.write(f"{stream_text}")
-        
-        
-with form:       
-    if not st.session_state.submitted:
-        st.title("Live Contract Risk & Obligation Scanner")
-        st.subheader("Upload File for Scanning")
-        uploaded=st.file_uploader(
+    
+    st.session_state.output=stream_text
+    st.session_state.processing=False
+    st.rerun()
+    
+
+              
+if not st.session_state.submitted:
+    st.title("Live Contract Risk & Obligation Scanner")
+    st.subheader("Upload File for Scanning")
+    uploaded=st.file_uploader(
             "Upload File",
             type=["pdf","txt","docx"],
             )
-        filetype=st.radio("FILE TYPE",options=["text-pdf","image-pdf","text-file"],index=None)
-        if filetype and uploaded:
-            st.write(f" Done type:{filetype}")
+    filetype=st.radio("FILE TYPE",options=["text-pdf","image-pdf","text-file"],index=None)
+    
+    if st.button("CHECK") and uploaded and filetype:
+        st.session_state.uploaded=uploaded
+        st.session_state.file_type=filetype
+        st.session_state.submitted=True
+        st.session_state.processing=True
+        st.rerun()
+
+def Reload():
+    keys=["output",'processing','submitted','uploaded','file_type']
+    for key in keys:
+        if key in st.session_state:
+            del st.session_state[key]
+    
+
+if st.session_state.output:
+    st.write(st.session_state.output)
+    st.button("Next Document",on_click=Reload)
+
+
         
-        st.button("CHECK",on_click=Request, args=(uploaded,filetype))
  
     
 
