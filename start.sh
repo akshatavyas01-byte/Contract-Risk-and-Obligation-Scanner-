75d42aa94fff8cf40428cafbb3c7547fbf01c9aa
AKSHATA VYAS <akshatavyas01@gmail.com>
2026-03-02 13:56:31 +0530

    Modify streamlit command to use dynamic port
    Updated streamlit command to use environment variable for port.

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 uvicorn main:app --host 0.0.0.0 --port 8000 &
-streamlit run streamlit_ui.py --server.port 8051 --server.address 0.0.0.0
+streamlit run streamlit_ui.py \
+--server.port $PORT \
+--server.address 0.0.0.0
