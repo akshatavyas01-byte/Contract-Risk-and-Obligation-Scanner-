7d219b0372c25bcade3a628eecb1678815629206
Akshata Vyas <akshatavyas01@gmail.com>
2026-03-09 12:19:19 +0530

    dockerfile and start.sh updated for packages.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-streamlit run streamlit_ui.py --server.port 8051 --server.address 0.0.0.0
+uvicorn src.main:app --host 0.0.0.0 --port 8000 &
+streamlit run src/streamlit_ui.py --server.port 8051 --server.address 0.0.0.0
